e2129d338eb8f79eff230a1faa21a7bd0711f3d7
Rafael”git commit -m ”curs 1” git config --global user.name Rafael” <rafaeldirle@yahoo.com>
2020-11-18 17:50:24 +0200

    ”tema”

---
+++ javatema.js
@@ -0,0 +1,17 @@
+var papagal = {
+    name : "Paco",
+    height : 10,
+    color : "blue"
+    
+}
+
+
+function primafunctie (r)
+
+{   
+    var r="Rafael";
+    return "Buna, numele meu este " +  r;
+   
+    
+}
+  console.log(primafunctie());
